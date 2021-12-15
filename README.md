54565d6dc06c5d072c6df8f722e3f35ef672a932
Yahya NOUALI <y.nouali@gmail.com>
2021-12-15 14:09:54 +0100

    config powershell for windows

---
+++ README.md
@@ -0,0 +1,2 @@
+All the available themes are here  
+- [Themes](https://ohmyposh.dev/docs/themes)
---
+++ user_profile.ps1
@@ -0,0 +1,31 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Load prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }	
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'iamya.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
