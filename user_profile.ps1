6232d0db0dad16c23fa42738222fe5538e826d82
Yahya NOUALI <y.nouali@gmail.com>
2022-03-04 12:22:04 +0100

    Update user_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -15,6 +15,7 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Alias
 Set-Alias vim nvim
